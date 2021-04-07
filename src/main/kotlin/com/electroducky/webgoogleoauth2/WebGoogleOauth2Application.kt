6c4da9bc7f477ec027ea1a4da1ab9d9de0c1fcc2
Electroducky <pu.alexa@yandex.ru>
2021-04-07 11:27:22 +0300

    init

---
+++ src/main/kotlin/com/electroducky/webgoogleoauth2/WebGoogleOauth2Application.kt
@@ -0,0 +1,11 @@
+package com.electroducky.webgoogleoauth2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebGoogleOauth2Application
+
+fun main(args: Array<String>) {
+    runApplication<WebGoogleOauth2Application>(*args)
+}
